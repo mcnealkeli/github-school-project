d7f0383986fc68d94d76cc8ce09d3bacfb1df85d
mcnealkeli <mcneal.keli@gmx.com>
2025-03-05 15:41:56 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class RandomJavaCode {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    System.out.println("Hello World");
+    if (x > 10) {
+      System.out.println("x is greater than 10");
+    } else {
+      System.out.println("x is less than or equal to 10");
+    }
+  }
+}
